50db7bcf0521f3153f1c13f4c8800c379cff765e
Carlos Alberto García de Alba Chávez <carlos.alberto.gdea.ch@gmail.com>
2020-02-03 14:02:18 -0600

    Created the backbone of the project

---
+++ src/main/java/com/itcg/nutritionalgames/NutritionalgamesApplication.java
@@ -0,0 +1,13 @@
+package com.itcg.nutritionalgames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutritionalgamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutritionalgamesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itcg/nutritionalgames/NutritionalgamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itcg.nutritionalgames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutritionalgamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
